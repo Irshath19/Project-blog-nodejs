11d035134b40056b7ef60b276853fc4176273900
Irshath19 <irshaththeraider1@gmail.com>
2024-06-30 14:40:50 +0530

    add

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -57,25 +57,25 @@ router.get('/admin', async (req, res) => {
 //   }
 // });
 
-router.post('/register', async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 10);
+// router.post('/register', async (req, res) => {
+//     try {
+//       const { username, password } = req.body;
+//       const hashedPassword = await bcrypt.hash(password, 10);
   
-      try {
-        const user = await User.create({ username, password:hashedPassword });
-        res.status(201).json({ message: 'User Created', user });
-      } catch (error) {
-        if(error.code === 11000) {
-          res.status(409).json({ message: 'User already in use'});
-        }
-        res.status(500).json({ message: 'Internal server error'})
-      }
+//       try {
+//         const user = await User.create({ username, password:hashedPassword });
+//         res.status(201).json({ message: 'User Created', user });
+//       } catch (error) {
+//         if(error.code === 11000) {
+//           res.status(409).json({ message: 'User already in use'});
+//         }
+//         res.status(500).json({ message: 'Internal server error'})
+//       }
   
-    } catch (error) {
-      console.log(error);
-    }
-  });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
 
 // router.get('/dashboard',authMiddleware,async(req,res)=>{
 //     res.render('admin/dashboard');
